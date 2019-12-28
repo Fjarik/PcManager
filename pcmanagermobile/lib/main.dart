81c56ccf6b9e5985dff9b34d9855551f5ffaabb3
Jiri Falta <jirkafalta@gmail.com>
2019-12-28 23:55:09 +0100

    Mobile send

--- pcmanagermobile/lib/main.dart
+++ pcmanagermobile/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -44,68 +46,121 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController _txtFirst = TextEditingController();
+  TextEditingController _txtSecond = TextEditingController();
+  TextEditingController _txtThird = TextEditingController();
+  TextEditingController _txtFourth = TextEditingController();
+  TextEditingController _txtPort = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String first = "192";
+  String second = "168";
+  String third = "50";
+  String fourth = "140";
+  String port = "7314";
+
+  String get ip => "${this.first}.${this.second}.${this.third}.${this.fourth}";
+
+  _MyHomePageState() {
+    _txtFirst.text = first;
+    _txtSecond.text = second;
+    _txtThird.text = third;
+    _txtFourth.text = fourth;
+    _txtPort.text = port;
+  }
+
+  void _connectPress() async {
+    var p = int.parse(port);
+    var socket = await Socket.connect(ip, p);
+
+    print('Connected to: '
+        '${socket.remoteAddress.address}:${socket.remotePort}');
+    socket.write("Test");
+    socket.destroy();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
+                    autocorrect: false,
+                    autofocus: false,
+                    controller: _txtFirst,
+                    decoration: InputDecoration(),
+                    keyboardType: TextInputType.number,
+                    maxLength: 3,
+                    minLines: 1,
+                    maxLines: 1,
+                  ),
+                ),
+                Flexible(
+                  child: TextField(
+                    autocorrect: false,
+                    autofocus: false,
+                    controller: _txtSecond,
+                    decoration: InputDecoration(),
+                    keyboardType: TextInputType.number,
+                    maxLength: 3,
+                    minLines: 1,
+                    maxLines: 1,
+                  ),
+                ),
+                Flexible(
+                  child: TextField(
+                    autocorrect: false,
+                    autofocus: false,
+                    controller: _txtThird,
+                    decoration: InputDecoration(),
+                    keyboardType: TextInputType.number,
+                    maxLength: 3,
+                    minLines: 1,
+                    maxLines: 1,
+                  ),
+                ),
+                Flexible(
+                  child: TextField(
+                    autocorrect: false,
+                    autofocus: false,
+                    controller: _txtFourth,
+                    decoration: InputDecoration(),
+                    keyboardType: TextInputType.number,
+                    maxLength: 3,
+                    minLines: 1,
+                    maxLines: 1,
+                  ),
+                ),
+                Flexible(
+                  child: TextField(
+                    autocorrect: false,
+                    autofocus: false,
+                    controller: _txtPort,
+                    decoration: InputDecoration(),
+                    keyboardType: TextInputType.number,
+                    maxLength: 4,
+                    minLines: 1,
+                    maxLines: 1,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            MaterialButton(
+              child: Text("Připojit"),
+              onPressed: _connectPress,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
